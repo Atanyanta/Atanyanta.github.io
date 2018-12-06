c7b0026230efb03efed7185356975fc0546adf88
Nick Roth <atanyanta@gmail.com>
2018-12-07 07:25:33 +1100

    spacing and json string fixes
    Fixed excessive spacing between objects, and cleaned up the formatting of json strings in the output tests.

--- main.js
+++ main.js
@@ -1,55 +1,62 @@
 var test, json, resultbox, ignored, keys, teststring, result;
 
 function dothething() {
-  test = document.getElementById("test").value;
-  json = JSON.parse(test);
-  resultbox = document.getElementById("results");
-  ignored = document.getElementById("ignored").value.split(" ");
+    test = document.getElementById("test").value;
+    json = JSON.parse(test);
+    resultbox = document.getElementById("results");
+    ignored = document.getElementById("ignored").value.split(" ");
 
-  resultbox.value = "";
-  resultbox.value +=
-    'tests["Status code is 200"] = responseCode.code === 200;\n';
-  resultbox.value += "var jsonData = JSON.parse(responseBody);\n";
-  if (json.length != undefined) {
-      resultbox.value +=
-    'tests["Data has correct length"] = jsonData.length = ' + json.length + ";\n\n";
-  }
+    resultbox.value = "";
+    resultbox.value +=
+        'tests["Status code is 200"] = responseCode.code === 200;\n';
+    resultbox.value += "var jsonData = JSON.parse(responseBody);\n";
+    if (json.length != undefined) {
+        resultbox.value +=
+            'tests["Data has correct length"] = jsonData.length = ' + json.length + ";\n\n";
+    }
+    else resultbox.value += "\n";
 
-  parse(json, "jsonData");
+    parse(json, "jsonData");
+}
+
+function schemagen(blob) {
+    
 }
 
 //much better recursive parsing function
 function parse(blob, locstring) {
-  keys = Object.keys(blob);
-  keys.forEach(function(element) {
-    if (typeof(blob[element]) == "object" && !!blob[element]) {
-      resultbox.value += "\n";
-      parse(blob[element], locstring + "['" + element + "']");
-    } else if (Array.isArray(blob[element])) {
-      resultbox.value += "\n";
-      parse(blob[element], locstring + "['" + element + "']");
-    } else if (!ignored.includes(element)) {
-      if (blob[element] == null) {
-        result = "null";
-      } else if (typeof(blob[element]) == "boolean") {
-        result = blob[element];
-      } else if (typeof(blob[element]) == "string") {
-        result = '"' + blob[element] + '"';
-      } else result = blob[element];
+    keys = Object.keys(blob);
+    keys.forEach(function (element, idx) {
+        if (typeof (blob[element]) == "object" && !!blob[element]) {
+            parse(blob[element], locstring + (isNaN(element) ? "." : "[") + element + (isNaN(element) ? "" : "]"));
+        } else if (Array.isArray(blob[element])) {
+            parse(blob[element], locstring + (isNaN(element) ? "." : "[") + element + (isNaN(element) ? "" : "]"));
+        } else if (!ignored.includes(element)) {
+            if (blob[element] == null) {
+                result = "null";
+            } else if (typeof (blob[element]) == "boolean") {
+                result = blob[element];
+            } else if (typeof (blob[element]) == "string") {
+                result = '"' + blob[element] + '"';
+            } else result = blob[element];
 
-      teststring =
-        'tests["Correct ' +
-        element +
-        " in " +
-        locstring +
-        '"] = ' +
-        locstring +
-        "['" +
-        element +
-        "'] === " +
-        result +
-        ";";
-      resultbox.value += teststring + "\n";
-    }
-  });
+            teststring =
+                'tests["Correct ' +
+                element +
+                ' in ' +
+                locstring +
+                '"] = ' +
+                locstring +
+                (isNaN(element) ? "." : "[") +
+                element +
+                (isNaN(element) ? "" : "]") +
+                " === " +
+                result +
+                ";";
+            resultbox.value += teststring + "\n";
+            if (idx == keys.length - 1) {
+                resultbox.value += "\n";
+            }
+        }
+    });
 }
