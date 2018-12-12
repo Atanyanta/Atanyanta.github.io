cd26929f256171a72aa2ec4c25eb669fb68aea16
Nick Roth <atanyanta@gmail.com>
2018-12-13 04:23:12 +1100

    Adding test for empty arrays/objects
    Added testing for length of object keys array and arrays by default. Added checkbox to ignore empty arrays/objects and not generate those tests.

--- main.js
+++ main.js
@@ -20,27 +20,58 @@ function dothething() {
 }
 
 function schemagen(blob) {
-    
+
 }
 
 //much better recursive parsing function
 function parse(blob, locstring) {
     keys = Object.keys(blob);
+
+    //loop through all keys
     keys.forEach(function (element, idx) {
-        if (typeof (blob[element]) == "object" && !!blob[element]) {
-            parse(blob[element], locstring + (isNaN(element) ? "." : "[") + element + (isNaN(element) ? "" : "]"));
-        } else if (Array.isArray(blob[element])) {
-            parse(blob[element], locstring + (isNaN(element) ? "." : "[") + element + (isNaN(element) ? "" : "]"));
-        } else if (!ignored.includes(element)) {
+        //if it's an array, recursively call the function on all array elements
+        if (Array.isArray(blob[element])) {
+            if (blob[element].length != 0 || document.getElementById("ignoreemptyarrays").checked) {
+                parse(blob[element], locstring + (isNaN(element) ? "." : "[") + element + (isNaN(element) ? "" : "]"));
+                return;
+            }
+        }
+        //same if it's an object (array is parsed first because an array is an object and will trigger this)
+        else if (typeof (blob[element]) == "object" && !!blob[element]) {
+            if (Object.keys(blob[element]) != 0 || document.getElementById("ignoreemptyarrays").checked){
+                console.log(blob[element]);
+                parse(blob[element], locstring + (isNaN(element) ? "." : "[") + element + (isNaN(element) ? "" : "]"));
+                return;
+            }
+        //if it's neither then we must be at an individual key:value pair
+        }  if (!ignored.includes(element)) {
             if (blob[element] == null) {
                 result = "null";
             } else if (typeof (blob[element]) == "boolean") {
                 result = blob[element];
             } else if (typeof (blob[element]) == "string") {
                 result = '"' + blob[element] + '"';
-            } else result = blob[element];
+            }
+            else result = blob[element];
 
-            teststring =
+            if (typeof (blob[element]) == "object" && !!blob[element]){
+                teststring =
+                'tests["Empty Array/Object ' +
+                element +
+                ' at ' +
+                locstring +
+                '"] = ' +
+                (Array.isArray(blob[element]) ? "" : "Object.keys(") +
+                locstring +
+                (isNaN(element) ? "." : "[") +
+                element +
+                (isNaN(element) ? "" : "]") +
+                (Array.isArray(blob[element]) ? ".length" : ").length") +
+                " === 0;";
+            }
+
+            else {
+                teststring =
                 'tests["Correct ' +
                 element +
                 ' in ' +
@@ -53,6 +84,7 @@ function parse(blob, locstring) {
                 " === " +
                 result +
                 ";";
+            }
             resultbox.value += teststring + "\n";
             if (idx == keys.length - 1) {
                 resultbox.value += "\n";
