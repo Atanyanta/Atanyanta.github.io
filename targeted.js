f6688eaee2f8cb0b52590c66a05e990205f11383
Nick Roth <nick.roth@qgenda.com>
2020-01-09 08:13:56 +1100

    Updated target generator
    included search term for exact parameter

--- targeted.js
+++ targeted.js
@@ -1,24 +1,37 @@
-var test, json, resultbox, keyid, keys, teststring, result;
+var test, json, resultbox, keyid, paramlookup, keys, teststring, result;
 
 function targetgen() {
     test = document.getElementById("target").value;
     json = JSON.parse(test);
     resultbox = document.getElementById("targetresults");
     keyid = document.getElementById("keyid").value;
+    paramlookup = document.getElementById("paramlookup").value;
 
-    if (!Array.isArray(json)) {
+    if (!Array.isArray(json) && paramlookup == "") {
         resultbox.value = "Response is not a valid array of objects.";
         return 0;
     }
 
     resultbox.value = "";
     resultbox.value += 'const utils = eval(globals.loadUtils);\n';
-    for (var i = 0; i < json.length; i++) {
-        if (Object.values(json[i]).includes(keyid)) {
-            resultbox.value += "var props = " + JSON.stringify(Object.keys(json[i])) + ";\n";
-            resultbox.value += "var expected = " + JSON.stringify(Object.values(json[i])) + ";\n";
-            resultbox.value += "utils.arrayCompareTest(null,null,props,expected);\n";
-            break;
+    if (paramlookup == "") {
+        for (var i = 0; i < json.length; i++) {
+            if (Object.values(json[i]).includes(keyid)) {
+                resultbox.value += "var props = " + JSON.stringify(Object.keys(json[i])) + ";\n";
+                resultbox.value += "var expected = " + JSON.stringify(Object.values(json[i])) + ";\n";
+                resultbox.value += "utils.arrayCompareTest(null,null,props,expected);\n";
+                break;
+            }
+        }
+    }
+    else {
+        for (var i = 0; i < json[paramlookup].length; i++) {
+            if (Object.values(json[paramlookup][i]).includes(keyid)) {
+                resultbox.value += "var props = " + JSON.stringify(Object.keys(json[paramlookup][i])) + ";\n";
+                resultbox.value += "var expected = " + JSON.stringify(Object.values(json[paramlookup][i])) + ";\n";
+                resultbox.value += "utils.arrayCompareTest(null,null,props,expected);\n";
+                break;
+            }
         }
     }
 }
