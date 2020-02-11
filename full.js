16681088dea15ec28f964ebcf2b0c8544506fbf7
Nick Roth <nick.roth@qgenda.com>
2020-02-12 03:43:39 +1100

    fixed parsefull to ignore fields
    fixed parsefull to ignore fields

--- full.js
+++ full.js
@@ -4,16 +4,17 @@ function fullgen() {
     test = document.getElementById("fulltest").value;
     json = JSON.parse(test);
     resultbox = document.getElementById("fullresults");
-    ignoredfields = document.getElementById("fullignoreemptyarrays").value;
+    ignoredfields = document.getElementById("fullignored").value;
 
     resultbox.value = '';
-    resultbox.value += 'var testStatus = "fail";\n';
+    resultbox.value += "postman.setEnvironmentVariable('testStatus','fail');\n";
     resultbox.value += 'let utils = eval(environment.utils)();\n';
     resultbox.value += 'var jsonData = JSON.parse(responseBody);\n';
     resultbox.value += 'var expected = ' + JSON.stringify(json) + '\n\n';
 
-    resultbox.value += 'pm.test("Full response is correct", function() {pm.expect(jsonData).to.eql(expected); testStatus="pass";});\n';
+    resultbox.value += 'pm.test("Full response is correct", function() {pm.expect(jsonData).to.eql(expected); postman.setEnvironmentVariable("testStatus","pass");});\n';
+    resultbox.value += 'var testStatus = pm.environment.get("testStatus");\n';
     resultbox.value += 'if (testStatus === "fail") {\n';
-    resultbox.value += '    utils.utils.parseFull(jsonData, "", expected);\n';
+    resultbox.value += '    utils.utils.parseFull(jsonData, "", expected, "' + ignoredfields + '");\n';
     resultbox.value += '}';
 }
